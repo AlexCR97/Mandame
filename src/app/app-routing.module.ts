46c9366503e8a8a6b81c139a6c1f7d21e252fdc4
uriel-panda <uriel.martinez.1998@outlook.com>
2020-03-30 13:33:52 -0600

    movimiento de carpeta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
     path: 'splash',
-    loadChildren: () => import('./splash/splash.module').then( m => m.SplashPageModule)
+    loadChildren: () => import('./page/splash/splash.module').then( m => m.SplashPageModule)
   },
 
 ];
